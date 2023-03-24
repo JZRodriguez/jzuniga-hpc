fb169e51ce8b6b9025df30388d3def3313a58643
Jadiel Zuniga <zujadiel@gmail.com>
2023-03-24 11:02:22 -0600

    Code

--- hello-mpi.c
+++ hello-mpi.c
@@ -23,43 +23,44 @@ int main(int argc, char **argv) {
 	y = x*x;
 	//printf("%i:%f^2 = %f\n", miproc, x, y);
 
-	if (miproc == 0) {
-	  sum = 1;
+	if (miproc == 0) { //master
+	  //sum = 1;
 	  for (int i = 1; i < numproc; i++) {
 	    data = i + 1;
-	    printf("Sending: %i -> %i, %f:", 0, i, data);
-	    MPI_Send(&data, sizeof(data), MPI_FLOAT, i, 98, MPI_COMM_WORLD);
-	    printf("OK.\n");
+	    //printf("Sending: %i -> %i, %f:", 0, i, data);
+	    MPI_Send(&data, sizeof(data), MPI_CHAR, i, 98, MPI_COMM_WORLD);
+	    //printf("OK.\n");
 	  }
 	MPI_Barrier (MPI_COMM_WORLD);
 	} else { //slaves
 	  //printf("Receiving%i -> %i:", 0, miproc);
-	  MPI_Recv(&data, sizeof(data), MPI_FLOAT, 0, 98, MPI_COMM_WORLD, &status);
+	  MPI_Recv(&data, sizeof(data), MPI_CHAR, 0, 98, MPI_COMM_WORLD, &status);
 	  //printf("%f: OK.\n", data);
 	  data = data * data;
 	  //MPI_Barrier (MPI_COMM_WORLD);
 	}
 
+	MPI_Barrier (MPI_COMM_WORLD);
 	
 
-	printf("Computing......\n");
+	// printf("Computing......\n");
 	
 	if (miproc == 0) {
 	  int k;
-	  printf("Master %i\n", numproc);
+	  //printf("Master %i\n", numproc);
 	  k = 0;
-	  for (j = 1; j < numproc; j++) {
+	  for (k = 1; k < numproc; k++) {
 	    k = k + 1;
-	    printf("RCV %i -> %i\n", k, 0);
-	    MPI_Recv(&data, sizeof(data), MPI_FLOAT, k, 99, MPI_COMM_WORLD, &status);
+	    //printf("RCV %i -> %i\n", k, 0);
+	    MPI_Recv(&data, sizeof(data), MPI_CHAR, k, 99, MPI_COMM_WORLD, &status);
 	    sum += data;
 	  }
 	  
 	  printf("Suma = %f\n", sum);
-	  MPI_Barrier (MPI_COMM_WORLD);
+	  //MPI_Barrier (MPI_COMM_WORLD);
 	} else {
-	  printf("SND %i -> %i\n", miproc, 0);
-	  MPI_Send(&data, sizeof(data), MPI_FLOAT, 0, 99, MPI_COMM_WORLD);
+	  //printf("SND %i -> %i\n", miproc, 0);
+	  MPI_Send(&data, sizeof(data), MPI_CHAR, 0, 99, MPI_COMM_WORLD);
 	  //MPI_Barrier (MPI_COMM_WORLD);
 	}
 	
